4ca9937c17b9b2478200ae0a7a726d084e02d754
Josh <joshuajtudball2003@gmail.com>
2022-12-08 14:02:04 +0000

    Forgot to add comments.

--- Roblox Universe ID Finder/Form1.cs
+++ Roblox Universe ID Finder/Form1.cs
@@ -1,18 +1,17 @@
+// Imports.
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Text.Json.Serialization;
 
-namespace Roblox_Universe_ID_Finder
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+namespace Roblox_Universe_ID_Finder // Namespace.
+{ // Opening of namespace.
+    public partial class Form1 : Form // The partial class form1 will inherit form to make a window.
+    { // Opening of partial class.
+        public Form1() // Form1s main constructor.
+        { // Opening of constructor.
+            InitializeComponent(); // Call initializecomponent, sets up window and controls.
+        } // Closing of constructor.
 
         private async void button1_Click(object sender, EventArgs e) // Runs when the user clicks the find button.
-        {
+        { // Opening of void.
 
             string input = InputBox.Text; // Get input from input box.
 
@@ -24,10 +23,10 @@ namespace Roblox_Universe_ID_Finder
 
             } // Closing of if statement.
 
-            HttpClient client = new HttpClient();
+            HttpClient client = new HttpClient(); // Create new http client to interact with roblox api.
 
-            try
-            {
+            try // Enter try to handle exceptions.
+            { // Opening of try.
 
                 string requestURL = "https://apis.roblox.com/universes/v1/places/" + input + "/universe"; // Build request url with input.
 
@@ -35,31 +34,32 @@ namespace Roblox_Universe_ID_Finder
 
                 response.EnsureSuccessStatusCode(); // Ensures success status code so response is guaranteed beyond this point.
 
-                string responseContent = await response.Content.ReadAsStringAsync();
+                string responseContent = await response.Content.ReadAsStringAsync(); // Read response content as string.
 
-                var responseJSON = JsonConvert.DeserializeObject<ResponseJSON>(responseContent);
+                var responseJSON = JsonConvert.DeserializeObject<ResponseJSON>(responseContent); // Deserialize response content with responsejson class.
 
-                if(responseJSON == null)
-                {
+                if(responseJSON == null) // If deserialized response is null.
+                { // Opening of if statement.
 
-                    throw new Exception("Response JSON is null.");
+                    throw new Exception("Response JSON is null."); // Throw a new exception indicating that response json is null.
 
-                }
-                else
-                {
+                } // Closing of if statement.
+                else // In any other case.
+                { // Opening of else statement.
 
-                    Clipboard.SetText(responseJSON.universeID.ToString());
-                    MessageBox.Show("Success! Your Universe ID is " + responseJSON.universeID + ", it's been copied to your clipboard.");
+                    Clipboard.SetText(responseJSON.universeID.ToString()); // Set clipboard text to the universe id converted to string.
+                    MessageBox.Show("Success! Your Universe ID is " + responseJSON.universeID + ", it's been copied to your clipboard."); // Show message that universe id was found and its been copied to clipboard.
 
-                }
+                } // Closing of else statement.
 
-            }catch(Exception err)
-            {
+            } // Closing of try.
+            catch(Exception err) // If error, catch exception.
+            { // Opening of catch.
 
-                MessageBox.Show($"An error occurred while trying to find your Universe ID. {err.Message}");
+                MessageBox.Show($"An error occurred while trying to find your Universe ID. {err.Message}"); // Show message that error occurred and provide error.
 
-            }
+            } // Closing of catch.
 
-        }
-    }
-}
+        } // Closing of void.
+    } // Closing of partial class.
+} // Closing of namespace.
--- Roblox Universe ID Finder/ResponseJSON.cs
+++ Roblox Universe ID Finder/ResponseJSON.cs
@@ -1,11 +1,13 @@
-﻿namespace Roblox_Universe_ID_Finder
-{
+﻿// Class for http request response.
 
-    public class ResponseJSON
-    {
+namespace Roblox_Universe_ID_Finder // Namespace.
+{ // Opening of namespace.
 
-        public long universeID { get; set; }
+    public class ResponseJSON // Class response json will be used to interpret the response from roblox api properly.
+    { // Opening of class.
 
-    }
+        public long universeID { get; set; } // Create public long for universe ID.
 
-}
+    } // Closing of class.
+    
+} // Closing of namespace.
